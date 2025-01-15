0c60b92adf5c981018cb4b40137321fd5c08857e
manoharhylancer <manoharvirati@gmail.com>
2025-01-15 10:26:17 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { redirect } from 'next/navigation'
 
 export default async function Home({ searchParams }: { searchParams: { design?: string } }) {
-  const { design = 'minimalist' } = await searchParams
+  const params = await searchParams;
+  const { design = 'minimalist' } = await params;
   
   // Redirect to the appropriate design
-  redirect(`/home/designs/${design}`)
+  redirect(`/home/designs/${design}`);
 }
 
